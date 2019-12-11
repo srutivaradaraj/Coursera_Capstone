92309822fcc346cf5adfaa0cdf37c18767b0c332
srutivaradaraj <58172628+srutivaradaraj@users.noreply.github.com>
2019-12-11 11:54:42 +0530

    Add files via upload

--- python_code.py
+++ python_code.py
@@ -1,7 +1,6 @@
 # Capstone Notebook
-
-### This notebook will be used to work through this course
+### This notebook will be used to work through the capstone course
 
 import pandas as pd
 import numpy as np
-print(" Hello Capstone Project Course")
+print ("Hello Capstone Project Course")
