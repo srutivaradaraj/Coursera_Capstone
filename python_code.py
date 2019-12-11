7242e5d1d74e0eb9d31b44180e68b0c3b0cee8e3
Sruti Varadaraj --CNTR <Sruti_Varadaraj@contractor.amat.com>
2019-12-11 11:38:07 +0530

    Coursera project notebook

--- python_code.py
+++ python_code.py
@@ -1,1 +1,7 @@
-print “Hello Github!”
+# Capstone Notebook
+
+### This notebook will be used to work through this course
+
+import pandas as pd
+import numpy as np
+print(" Hello Capstone Project Course")
